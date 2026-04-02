7e565290f6c277a485d79bcf4b36601da594c378
Hi, I am Sandeep B <sandeep16b@gmail.com>
2026-04-02 07:52:39 -0500

    fd

--- ClientApp/src/components/SearchReceivableReport.js
+++ ClientApp/src/components/SearchReceivableReport.js
@@ -267,7 +267,7 @@ export class SearchReceivableReport extends Component {
         {/* Search Form */}
         <Row className="mb-4">
           <Col>
-            <Card>
+            <Card className="modern-card">
               <CardHeader><h5>Search Criteria</h5></CardHeader>
               <CardBody>
                 <Form>
@@ -363,7 +363,7 @@ export class SearchReceivableReport extends Component {
         {/* AG-Grid Results */}
         <Row>
           <Col>
-            <Card>
+            <Card className="modern-card results-card">
               <CardHeader>
                 <div className="d-flex justify-content-between align-items-center">
                   <h5 className="mb-0">Search Results</h5>
